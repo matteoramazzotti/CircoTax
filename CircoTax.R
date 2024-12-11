6a7ad7918ee2ac0b0f12cafa638e1ed1e6a91e93
Matteo Ramazzotti <matteo.ramazzotti@unifi.it>
2024-12-11 12:08:14 +0100

    Update CircoTax.R

--- CircoTax.R
+++ CircoTax.R
@@ -1,118 +1,135 @@
-CircoTax=function(file,title="CircoTax plot",ramp=c("orange","white","blue"),tax_col=9:14,fc_col=3,sort=c("no","rank","fc","absfc","alpha"),sort_dir="d") {
-	library(ggplot2)
-	if(is.character(file)) {
-		data=read.delim(file,sep="\t")
-	} else {
-		data=file
-	}
-	sort_dir=ifelse(sort_dir == "d",FALSE,TRUE)
-	if(length(tax_col) == 6) { #KPCOFG as in RDP
-		gplot_labels= unlist(strsplit("KPCOFG",""))
-	}
-	if(length(tax_col) == 7) { #KPCOFGS as in silva
-		gplot_labels= unlist(strsplit("KPCOFGS",""))
-	}
-	
-	#build the taxa-related variables (tax index and label)
-	#ranks are assumed to be in decreasing order from kinkdom(domain) to species
-	#y represent the height (from the center of the circle) of the bars => domain=1 (min) species=7 (max)
-	y=apply(data,1,function(x) length(tax_col)-sum(ifelse(is.na(x[tax_col]),1,0)))
-	if(sort[1] == "no") {
-		#y=apply(data,1,function(x) length(tax_col)-sum(ifelse(is.na(x[tax_col]),1,0)))
-		synth=apply(data[,tax_col],1,function(x) paste0(x,collapse="-"))
-		labels=unlist(lapply(strsplit(gsub("-NA.*","",perl=T,synth),"-"),function(x) rev(x)[1]))
-		y=y
-		fc=data[,fc_col]
-	}
-	if(sort[1] == "rank") {
-		#y=apply(data,1,function(x) length(tax_col)-sum(ifelse(is.na(x[tax_col]),1,0)))
-		o=order(y,decreasing=sort_dir)
-		synth=apply(data[o,tax_col],1,function(x) paste0(x,collapse="-"))
-		labels=unlist(lapply(strsplit(gsub("-NA.*","",perl=T,synth),"-"),function(x) rev(x)[1]))
-		y=y[o]
-		fc=data[o,fc_col]
-	}
-	if(sort[1] == "alphalin") {
-		synth=apply(data[,tax_col],1,function(x) paste0(x,collapse="-"))
-		o=order(synth,decreasing=sort_dir)
-		synth=synth[o]
-		labels=unlist(lapply(strsplit(gsub("-NA.*","",perl=T,synth),"-"),function(x) rev(x)[1]))
-		y=y[o]
-		fc=data[o,fc_col]
-	}
-	if(sort[1] == "alpha") {
-		synth=apply(data[,tax_col],1,function(x) paste0(x,collapse="-"))
-		labels=unlist(lapply(strsplit(gsub("-NA.*","",perl=T,synth),"-"),function(x) rev(x)[1]))
-		o=order(labels)
-		labels=labels[o]
-		y=y[o]
-		fc=data[o,fc_col]
-	}
-	if(sort[1] == "fc") {
-		o=order(data[,fc_col],decreasing=sort_dir)
-		synth=apply(data[,tax_col],1,function(x) paste0(x,collapse="-"))
-		synth=synth[o]
-		labels=unlist(lapply(strsplit(gsub("-NA.*","",perl=T,synth),"-"),function(x) rev(x)[1]))
-		y=y[o]
-		fc=data[o,fc_col]
-	}
-	if(sort[1] == "absfc") {
-		#y=apply(data,1,function(x) length(tax_col)-sum(ifelse(is.na(x[tax_col]),1,0)))
-		o=order(abs(data[,fc_col]),decreasing=sort_dir)
-		synth=apply(data[o,tax_col],1,function(x) paste0(x,collapse="-"))
-		synth=synth[order(abs(data[,fc_col]),decreasing=sort_dir)]
-		labels=unlist(lapply(strsplit(gsub("-NA.*","",perl=T,synth),"-"),function(x) rev(x)[1]))
-		fc=data[o,fc_col]
-	}
-
-	#builds the data.frame for ggplot2 
-	df=data.frame("id"=1:dim(data)[1],"name"=labels,"rank"=y,"FC"=fc)
-
-	#adds the label angle column
-	nbar=dim(df)[1]
-	angle=90-360* ((1:nbar)-0.5) / nbar
-	angle<-ifelse(angle < -90, angle+180, angle)
-	df$angle=angle
-
-	#the plot starts here
-	ggplot(df, aes(x = id, y = rank)) +
-	ggtitle(title) +
-	geom_col(
-		aes(fill=FC),
-		position = "dodge"
-	) + 
-	scale_fill_gradient2(
-		low="blue",
-		mid="white",
-		high="orange"
-	) +
-	annotate(
-		#"text",label=rev(c("G","F","O","C","P","K")), x=rep(0,6), y=c(1,2,3,4,5,6),size=3
-		"text",label=gplot_labels, x=rep(0,length(tax_col)), y=1:length(tax_col),size=3
-	) +
-	geom_text(
-			aes(
-				x= id, 
-				y=7, 
-				label=name,
-			), 
-			color="black", 
-			fontface="bold",
-			alpha=0.6,
-			size=3,
-			angle=angle,
-	) +
-	geom_hline( 
-			yintercept=1:6,
-			color="grey"
-	) +
-	theme(
-		axis.text = element_blank(),
-		axis.ticks = element_blank(),
-		axis.title = element_blank(),
-		panel.background = element_rect(fill = NA),
-		plot.margin = unit(rep(1,4), "cm"),      # Adjust the margin to make in sort labels are not truncated!
-		plot.title = element_text(hjust = 0.5,face="bold", size=18)
-	) +
-	coord_polar(start = 0, clip="off")
+CircoTax=function(file,
+                  title="CircoTax plot",
+                  fill_text="fold change",
+                  ramp=c("orange","white","blue"),
+                  tax_col=2:length(colnames(file)),
+                  fc_col=1,
+                  sort=c("no","rank","fc","absfc","alpha"),
+                  sort_dir="d") {
+  
+  # version: 08/03/2024
+  
+  
+  data=file
+  
+  library("ggplot2")
+  library("ggh4x")
+  
+  sort_dir=ifelse(sort_dir == "d",FALSE,TRUE)
+  
+  if( "Domain" %in% colnames(file) | "Kingdom" %in% colnames(file) ){
+    file <- gsub("Kingdom","Domain",colnames(file))
+    if(length(tax_col) == 5) {
+      gplot_labels= unlist(strsplit("DPCOF",""))
+    }
+    if(length(tax_col) == 6) { #KPCOFG as in RDP
+      gplot_labels= unlist(strsplit("DPCOFG",""))
+    }
+    if(length(tax_col) == 7) { #KPCOFGS as in silva
+      gplot_labels= unlist(strsplit("DPCOFGS",""))
+    }
+  } else {
+    if(length(tax_col) == 5) {
+      gplot_labels= unlist(strsplit("PCOFG",""))
+    }
+    if(length(tax_col) == 6) { #PCOFGS
+      gplot_labels= unlist(strsplit("PCOFGS",""))
+    }
+  }
+    
+  #build the taxa-related variables (tax index and label)
+  #ranks are assumed to be in decreasing order from kinkdom(domain) to species
+  #y represent the height (from the center of the circle) of the bars => domain=1 (min) species=7 (max)
+  y=apply(data,1,function(x) length(tax_col)-sum(ifelse(is.na(x[tax_col]),1,0)))
+  if(sort[1] == "no") {
+    #y=apply(data,1,function(x) length(tax_col)-sum(ifelse(is.na(x[tax_col]),1,0)))
+    synth=apply(data[,tax_col],1,function(x) paste0(x,collapse="-"))
+    labels=unlist(lapply(strsplit(gsub("-NA.*","",perl=T,synth),"-"),function(x) rev(x)[1]))
+    y=y
+    fc=data[,fc_col]
+  }
+  if(sort[1] == "rank") {
+    #y=apply(data,1,function(x) length(tax_col)-sum(ifelse(is.na(x[tax_col]),1,0)))
+    o=order(y,decreasing=sort_dir)
+    synth=apply(data[o,tax_col],1,function(x) paste0(x,collapse="-"))
+    labels=unlist(lapply(strsplit(gsub("-NA.*","",perl=T,synth),"-"),function(x) rev(x)[1]))
+    y=y[o]
+    fc=data[o,fc_col]
+  }
+  if(sort[1] == "alphalin") {
+    synth=apply(data[,tax_col],1,function(x) paste0(x,collapse="-"))
+    o=order(synth,decreasing=sort_dir)
+    synth=synth[o]
+    labels=unlist(lapply(strsplit(gsub("-NA.*","",perl=T,synth),"-"),function(x) rev(x)[1]))
+    y=y[o]
+    fc=data[o,fc_col]
+  }
+  if(sort[1] == "alpha") {
+    synth=apply(data[,tax_col],1,function(x) paste0(x,collapse="-"))
+    labels=unlist(lapply(strsplit(gsub("-NA.*","",perl=T,synth),"-"),function(x) rev(x)[1]))
+    o=order(labels)
+    labels=labels[o]
+    y=y[o]
+    fc=data[o,fc_col]
+  }
+  if(sort[1] == "fc") {
+    o=order(data[,fc_col],decreasing=sort_dir)
+    synth=apply(data[,tax_col],1,function(x) paste0(x,collapse="-"))
+    synth=synth[o]
+    labels=unlist(lapply(strsplit(gsub("-NA.*","",perl=T,synth),"-"),function(x) rev(x)[1]))
+    y=y[o]
+    fc=data[o,fc_col]
+  }
+  if(sort[1] == "absfc") {
+    #y=apply(data,1,function(x) length(tax_col)-sum(ifelse(is.na(x[tax_col]),1,0)))
+    o=order(abs(data[,fc_col]),decreasing=sort_dir)
+    synth=apply(data[o,tax_col],1,function(x) paste0(x,collapse="-"))
+    synth=synth[order(abs(data[,fc_col]),decreasing=sort_dir)]
+    labels=unlist(lapply(strsplit(gsub("-NA.*","",perl=T,synth),"-"),function(x) rev(x)[1]))
+    fc=data[o,fc_col]
+  }
+  
+  #builds the data.frame for ggplot2 
+  df=data.frame("id"=1:dim(data)[1],"name"=labels,"rank"=y,"FC"=fc)
+  
+  #the plot starts here
+  ggplot(df, aes(x = id, y = rank)) +
+    ggtitle(title) +
+    geom_col(
+      aes(fill=FC),
+      position = "dodge"
+    ) + 
+    scale_fill_gradient2(
+      low="blue",
+      mid="white",
+      high="orange"
+    ) +
+    annotate(
+      "text",label=gplot_labels, x=rep(0,length(tax_col)), y=1:length(tax_col),size=4.5, vjust=0.58
+    ) +
+    # geom_text( 
+    geom_text_aimed(  # it allows to calculate the ideal angle to have labels perfectly perpendicular to the axis
+      aes(
+        x= id,
+        y=7,
+        label=name),
+      color="black",
+      fontface="bold",
+      alpha=0.6,
+      size=3
+    ) +
+    geom_hline( 
+      yintercept=1:length(tax_col),
+      color="grey"
+    ) +
+    coord_polar(start = 0, clip="off") +
+    theme(
+      axis.text = element_blank(),
+      axis.ticks = element_blank(),
+      axis.title = element_blank(),
+      panel.background = element_rect(fill = NA),
+      plot.margin = unit(rep(1,4), "cm"),      # It adjusts the margins of the plot to avoid the trimming of the labels!
+      plot.title = element_text(hjust = 0.5,face="bold", size=18)
+    ) +
+    labs(fill=fill_text)
 }
