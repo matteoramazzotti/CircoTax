959c502a202185da959b4d17036b54a0713d7792
Von B. <lorenzo.casbarra@gmail.com>
2025-07-10 15:44:35 +0200

    feat: new params for plot aesthethic

--- R/CircoTax.R
+++ R/CircoTax.R
@@ -10,14 +10,20 @@
 #' @param tax_col Column index or range of column indexes holding IDs for the ranks. Default: 2:length(colnames(input_table))
 #' @param fc_col Column index holding log2FC values. Default: 1 when tax_col argument is a range of indexes else 3 
 #' @param names Column index holding IDs for the taxon, used when tax_col argument is a single index. Default: 1
-#' @param size_taxon_circo Size of plot annotation. Default: 4
+#' @param size_taxon_circo Size of plot IDs annotation. Default: 4
+#' @param rank_ann_size Size of plot ranks annotation. Default: 4.5
+#' @param legend_height Height for legend bar (in pt) Default: 50
+#' @param legend_width Width for legend bar (in pt) Default: 18
+#' @param legend_title_size Legend title text size (in pt) Default: 15
+#' @param legend_text_size Legend scale text size (in pt) Default: 10
 #' @param sort Sorting logic: "no" (no sorting), "rank" (by taxonomic rank), "fc" (by fold change), "absfc" (by absolute fold change), "alpha" (alphabetic order), "alpharank" (). Default: "rank".
 #' @param rank_vector Character vector holding the ranking system ordered from Highest rank to lowest. Default: c("Domain","Phylum","Class","Order","Family","Genus","Species")
 #' @param decrease_order Boolean to invert order in sorting. Default: FALSE
 #' @param collapse_ranks Boolean to use only Ranks represented in the table. Default: TRUE
 #' @param save_path Save path for plot image. Default: NULL
+#' @param output_name Name for saved file. Default: "CircoTax_plot"
 #' @param image_dim Pixel value for height and width of the plot.  Default: 4000
-#' @param save_format "png" or "pdf". Default: "png"
+#' @param save_format "png" or "tiff" or "jpeg". Default: "png"
 #' @return CircoTax plot
 #' @export
 CircoTax = function (
@@ -28,22 +34,22 @@ CircoTax = function (
   tax_col = 2:length(colnames(input_table)),
   fc_col = ifelse(length(tax_col)>1,1,3),
   names = 1,
-  size_taxon_circo=4,
+  size_taxon_circo = 4,
+	rank_ann_size = 4.5,
+	legend_height = 50,
+	legend_width = 18,
+	legend_title_size = 15,
+	legend_text_size = 10,
   sort = c("rank","no","fc","absfc","alpha","alpharank"),
 	rank_vector = c("Domain","Phylum","Class","Order","Family","Genus","Species"),
   decrease_order = FALSE,
 	collapse_ranks = FALSE,
 	save_path = NULL,
+	output_name = "CircoTax_plot",
 	image_dim = 4000,
 	save_format = "png"
 ) {
-    
-  # library("ggplot2")
-  # library("ggh4x")
-  # library("stringi")
-	# library("here")
-
-
+  
 	ow <- options("warn")
 	options(warn = 0)
 	if (!length(ramp) == 3) {
@@ -108,7 +114,7 @@ CircoTax = function (
     label = gplot_rank_labels,
     x = rep(0,length(tax_col)),
     y = 1:length(tax_col),
-    size = 4.5,
+    size = rank_ann_size,
     vjust = 0.58
   )
 
@@ -129,9 +135,24 @@ CircoTax = function (
     color="grey"
   ) 
   circoClip = ggplot2::coord_polar(start = 0, clip="off")
-  circoLegendTitle = ggplot2::labs(fill=fill_text)
+  circoLegendTitle = ggplot2::labs(fill = fill_text)
 
   #the plot starts here
+	if (!is.null(save_path)) {
+		scale = image_dim / 4000
+		switch(
+			save_format,
+			tiff = {
+				ragg::agg_tiff(paste0(save_path,"/",output_name,".",save_format), width = image_dim, height = image_dim, units = "px", res = 300, scaling = scale)
+			},
+			jpeg = {
+				ragg::agg_jpeg(paste0(save_path,"/",output_name,".",save_format), width = image_dim, height = image_dim, units = "px", res = 300, scaling = scale)
+			},
+			{
+				ragg::agg_png(paste0(save_path,"/",output_name,".",save_format), width = image_dim, height = image_dim, units = "px", res = 300, scaling = scale)
+			}
+		)
+	}
 	plot(
 		ggplot2::ggplot(
 			df,
@@ -149,13 +170,13 @@ CircoTax = function (
 			axis.text = ggplot2::element_blank(),
 			axis.ticks = ggplot2::element_blank(),
 			axis.title = ggplot2::element_blank(),
+			legend.title = ggplot2::element_text(hjust=0.5, size = legend_title_size),
+			legend.text = ggplot2::element_text(size = legend_text_size),
+			legend.key.height = ggplot2::unit(50,"pt"),
+			legend.key.width = ggplot2::unit(18,"pt"),
 			panel.background = ggplot2::element_rect(fill = NA),
 			plot.margin = grid::unit(rep(1,4), "cm"),      # It adjusts the margins of the plot to avoid the trimming of the labels!
 			plot.title = ggplot2::element_text(hjust = 0.5,face="bold", size=18)
 		)
 	)
-	if (!is.null(save_path)) {
-		scale = 4000 /image_dim
-		ggplot2::ggsave(filename = paste0(save_path,"CircoTax_plot.",save_format), device=save_format, width = image_dim, height = image_dim, units = "px", dpi = 300,scale = scale)	
-	}
 }
